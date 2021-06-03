27a95cb7002292ed92fd143e7dd6501132a777eb
am.moving.lightspeed <am.moving.lightspeed@gmail.com>
2021-06-03 15:09:41 +0300

    Minor fixes.

---
+++ Caravan/js/building_deck/kb-events.js
@@ -0,0 +1,81 @@
+kbLayout = {
+    add: "KeyW",
+    remove: "KeyR",
+    randomize: "KeyX",
+    selectAll: "KeyA",
+    moveLeft: "ArrowLeft",
+    moveRight: "ArrowRight"
+};
+
+
+
+(function() {
+
+    let pickedCardsCounter = document.getElementById('picked-cards-counter');
+    let totalCardsCounter = document.getElementById('total-cards-counter');
+
+    document.addEventListener(
+        'keydown',
+        (e) => {
+            if (e.code === kbLayout['moveLeft']) {
+                shiftLeft();
+            }
+        }
+    );
+
+    document.addEventListener(
+        'keydown',
+        (e) => {
+            if (e.code === kbLayout['moveRight']) {
+                shiftRight();
+            }
+        }
+    );
+
+    document.addEventListener(
+        'keydown',
+        (e) => {
+            if (e.code === kbLayout['add'] && e.repeat != true) {
+
+                let areChanged = changeMatchingCards(
+                    this.curPos.absolute,
+                    (index) => this.availableCards[index] !== this.cardBack
+                );
+
+                if (areChanged) {
+                    pickedCardsCounter.innerText = Number.parseInt(pickedCardsCounter.innerText) + 1;
+                }
+            }
+        }
+    );
+
+    document.addEventListener(
+        'keydown',
+        (e) => {
+            if (e.code === kbLayout['remove'] && e.repeat != true) {
+
+                let areChanged =changeMatchingCards(
+                    this.curPos.absolute,
+                    (index) => this.pickedCards[index] !== this.cardBack
+                );
+
+                if (areChanged) {
+                    pickedCardsCounter.innerText = Number.parseInt(pickedCardsCounter.innerText) - 1;
+                }
+            }
+        }
+    );
+
+    document.addEventListener(
+        'keydown',
+        (e) => {
+            if (e.code === kbLayout['selectAll'] && e.repeat != true) {
+
+                exchangeDecks(this.curPos.absolute);
+                pickedCardsCounter.innerText =
+                    Number.parseInt(totalCardsCounter.innerText) -
+                    Number.parseInt(pickedCardsCounter.innerText);
+            }
+        }
+    );
+})();
